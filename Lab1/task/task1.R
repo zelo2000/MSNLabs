a8354469af3e537278ede3bd4450d50a93295df5
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2021-09-17 11:48:24 +0300

    add code for task 1 and start report for task 1

--- Lab1/task/task1.R
+++ Lab1/task/task1.R
@@ -1,7 +1,7 @@
 # 1.1
-setwd('D:\\LNU\\MSN\\Lab1\\task')
+setwd('D:\\LNU\\MSN\\MSNLabs\\Lab1\\task')
 college = read.csv('College.csv', header = T, na.string = '?')
-# print(college)
+print(college)
 
 # 1.2
 fix(college)
@@ -13,5 +13,26 @@ college = college[, -1]
 fix(college)
 
 # 1.3.1
-Summary = summary(college)
-print(Summary)
+summary(college)
+
+#1.3.2
+college$Private = as.factor(college$Private)
+pairs(college[1:10])
+
+#1.3.3
+plot(college$Private, college$Outstate)
+
+#1.3.4
+Elite = rep("No", nrow(college))
+Elite[college$Top10perc > 50] = " Yes"
+Elite = as.factor(Elite)
+college = data.frame(college ,Elite)
+summary(college)
+plot(college$Elit, college$Outstate)
+
+#1.3.5
+par(mfrow=c(2,2))
+hist(college$Expend)
+hist(college$Outstate)
+hist(college$PhD)
+hist(college$Terminal)
