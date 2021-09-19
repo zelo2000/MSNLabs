d5e67aefc3530555d935218b1484b090f8a99e19
Mykhailo <bordun.mykhailo24@gmail.com>
2021-09-20 00:40:05 +0300

    Finished 3rd task

--- Lab1/task/task3.R
+++ Lab1/task/task3.R
@@ -9,6 +9,26 @@ pairs(Boston)
 # 3.3
 pairs(Boston, verInd=1)
 
+cat("\n")
+
 # 3.4
 Boston$chas = as.factor(Boston$chas)
-summary(Boston)
+print(summary(Boston))
+
+cat("\n")
+
+# 3.5
+print(paste("Bounds river:", sum(Boston$chas == 1)))
+
+cat("\n")
+
+# 3.6
+print(paste("Median P/t ration:", median(Boston$ptratio)))
+
+cat("\n")
+
+# 3.7
+min_medv = Boston[Boston$medv == min(Boston$medv), ]
+print(min_medv)
+cat("\n")
+print(min_medv[,-4] - apply(Boston[,-4], 2, mean))
