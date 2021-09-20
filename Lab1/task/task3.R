9725403d6180db900d8e26ece4a6c8a6ce46cdf2
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2021-09-20 17:24:58 +0300

    task3 report

--- Lab1/task/task3.R
+++ Lab1/task/task3.R
@@ -4,27 +4,23 @@ Boston
 ?Boston
 
 # 3.2
-pairs(Boston)
+pairs(~age+tax+crim+rad+ptratio, Boston)
 
 # 3.3
 pairs(Boston, verInd=1)
-
 cat("\n")
 
 # 3.4
 Boston$chas = as.factor(Boston$chas)
 print(summary(Boston))
-
 cat("\n")
 
 # 3.5
 print(paste("Bounds river:", sum(Boston$chas == 1)))
-
 cat("\n")
 
 # 3.6
 print(paste("Median P/t ration:", median(Boston$ptratio)))
-
 cat("\n")
 
 # 3.7
