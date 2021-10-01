c525ce33b824f6983b948608a865950c1402b822
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2021-10-01 17:30:57 +0300

    add code for task1

---
+++ Lab2/tasks/task1.R
@@ -0,0 +1,18 @@
+# 1
+setwd('D:\\LNU\\MSN\\MSNLabs\\Lab2\\tasks')
+autos = read.csv('Auto.csv', header = T, na.string = '?')
+autos = na.omit(autos) 
+fix(autos)
+
+# 1.1
+lm.fit = lm(mpg~horsepower,data=autos)
+lm.fit
+summary(lm.fit)
+
+# 1.2
+plot(autos$mpg)
+abline(lm.fit)
+
+#1.3
+par(mfrow=c(2,2))
+plot(lm.fit)
