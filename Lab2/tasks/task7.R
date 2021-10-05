a95ab89cc9799e23b0fdd77752e3d9b6b25eadf1
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2021-10-05 21:46:31 +0300

    Update script

--- Lab2/tasks/task7.R
+++ Lab2/tasks/task7.R
@@ -4,8 +4,10 @@ x1=runif(100)
 x2 =0.5*x1+rnorm(100)/10
 y=2+2*x1+0.3*x2+rnorm(100)
 
+lm.fit = lm(y~x1+x2)
+summary(lm.fit)
+
+#7.2
 cor(x1, x2)
 plot(x1, x2)
 
-lm.fit = lm(y~x1+x2)
-summary(lm.fit)
