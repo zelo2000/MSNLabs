9717dae5867edf7d620a2b332caaad90ff017a23
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2021-11-21 23:11:59 +0200

    Lab6_Zelo task5

--- Lab6_Zelo/tasks/task5.R
+++ Lab6_Zelo/tasks/task5.R
@@ -1,1 +1,51 @@
 # 5
+
+# 5.1
+library(ISLR)
+library(leaps)
+set.seed(1)
+attach(College)
+train = sample(length(Outstate), length(Outstate) / 2)
+test = -train
+College.train = College[train, ]
+College.test = College[test, ]
+fit = regsubsets(Outstate ~ ., data = College.train, nvmax = 17, method = "forward")
+fit.summary = summary(fit)
+par(mfrow = c(1, 3))
+plot(fit.summary$cp, xlab = "Number of variables", ylab = "Cp", type = "l")
+min.cp = min(fit.summary$cp)
+std.cp = sd(fit.summary$cp)
+abline(h = min.cp + 0.2 * std.cp, col = "red", lty = 2)
+abline(h = min.cp - 0.2 * std.cp, col = "red", lty = 2)
+plot(fit.summary$bic, xlab = "Number of variables", ylab = "BIC", type='l')
+min.bic = min(fit.summary$bic)
+std.bic = sd(fit.summary$bic)
+abline(h = min.bic + 0.2 * std.bic, col = "red", lty = 2)
+abline(h = min.bic - 0.2 * std.bic, col = "red", lty = 2)
+plot(fit.summary$adjr2, xlab = "Number of variables", ylab = "Adjusted R2", type = "l", ylim = c(0.4, 0.84))
+max.adjr2 = max(fit.summary$adjr2)
+std.adjr2 = sd(fit.summary$adjr2)
+abline(h = max.adjr2 + 0.2 * std.adjr2, col = "red", lty = 2)
+abline(h = max.adjr2 - 0.2 * std.adjr2, col = "red", lty = 2)
+
+fit = regsubsets(Outstate ~ ., data = College, method = "forward")
+coeffs = coef(fit, id = 6)
+names(coeffs)
+
+# 5.2
+library(gam)
+fit = gam(Outstate ~ Private + s(Room.Board, df = 2) + s(PhD, df = 2) + s(perc.alumni, df = 2) + s(Expend, df = 5) + s(Grad.Rate, df = 2),
+	data=College.train)
+par(mfrow = c(2, 3))
+plot(fit, se = T, col = "blue")
+
+# 5.3
+preds = predict(fit, College.test)
+err = mean((College.test$Outstate - preds)^2)
+err
+tss = mean((College.test$Outstate - mean(College.test$Outstate))^2)
+rss = 1 - err / tss
+rss
+
+# 5.4
+summary(fit)
