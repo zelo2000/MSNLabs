8e45615c9cfc00e1e05b048d5e5c07e81c1e47d4
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2021-11-25 22:27:04 +0200

    finish lab 6 zelo

--- Lab6_Zelo/tasks/task6.R
+++ Lab6_Zelo/tasks/task6.R
@@ -1,1 +1,43 @@
 # 6
+
+# 6.1
+set.seed(1)
+X1 = rnorm(100)
+X2 = rnorm(100)
+eps = rnorm(100, sd = 0.1)
+Y = -3.14 + 2.72 * X1 + 0.5 * X2 + eps
+
+# 6.2
+beta0 = rep(0, 1000)
+beta1 = rep(0, 1000)
+beta2 = rep(0, 1000)
+beta1[1] = 7
+
+# 6.3-6.5
+for (i in 1:1000) {
+    a = Y - beta1[i] * X1
+    beta2[i] = lm(a ~ X2)$coef[2]
+    a = Y - beta2[i] * X2
+    lm.fit = lm(a ~ X1)
+    if (i < 1000) {
+        beta1[i + 1] = lm.fit$coef[2]
+    }
+    beta0[i] = lm.fit$coef[1]
+}
+plot(1:1000, beta0, type = "l", xlab = "iteration", ylab = "betas", ylim = c(-4, 3), col = "green")
+lines(1:1000, beta1, col = "red")
+lines(1:1000, beta2, col = "blue")
+legend("center", c("beta0", "beta1", "beta2"), lty = 1, col = c("green", "red", "blue"))
+
+# 6.6
+lm.fit = lm(Y ~ X1 + X2)
+plot(1:1000, beta0, type = "l", xlab = "iteration", ylab = "betas", ylim = c(-4, 3), col = "green")
+lines(1:1000, beta1, col = "red")
+lines(1:1000, beta2, col = "blue")
+abline(h = lm.fit$coef[1], lty = "dashed", lwd = 3, col = rgb(0, 0, 0, alpha = 0.4))
+abline(h = lm.fit$coef[2], lty = "dashed", lwd = 3, col = rgb(0, 0, 0, alpha = 0.4))
+abline(h = lm.fit$coef[3], lty = "dashed", lwd = 3, col = rgb(0, 0, 0, alpha = 0.4))
+legend("center", c("beta0", "beta1", "beta2", "multiple regression"), lty = c(1, 
+    1, 1, 2), col = c("green", "red", "blue", "black"))
+
+
